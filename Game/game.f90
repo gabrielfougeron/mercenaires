7193555ca484274fc78c75f5bb09f421ec9feac1
gabrielfougeron <gabriel.fougeron@hotmail.fr>
2015-03-21 10:20:17 +0100

    initial commit for mercenaires

---
+++ Game/game.f90
@@ -0,0 +1,3 @@
+program game
+
+end program game
---
+++ Stategies/strategies.f90
@@ -0,0 +1,9 @@
+module strategies
+
+    integer                 , parameter :: real_kind = 8        ! Precision du calcul
+    real(kind = real_kind)  , parameter :: one = 1
+
+contains
+
+
+end module strategies
