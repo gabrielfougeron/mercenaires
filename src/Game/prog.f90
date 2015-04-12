5a21e2356baba04ea2fbd6c68549a6bb1db19ee1
gabrielfougeron <gabriel.fougeron@hotmail.fr>
2015-04-12 23:58:28 +0200

    Weird seg fault

--- src/Game/prog.f90
+++ src/Game/prog.f90
@@ -1,59 +1,24 @@
 call init_random_seed()
 
-!~ call play_game(human,unif_distrib,winner,verball)
-!~ 
-!~ ngames = 10**7
-!~ 
-!~ call play_many_games(minusone,plusone,ngames,winnertable)
-!~ 
-!~ rwinnertable = real(winnertable,real_kind)
-!~ rwinnertable = rwinnertable / ngames 
-!~ 
-!~ print *,rwinnertable*100
-
-!~ 
-m = 2
-n = 2
-
-allocate(A(m,n))
-allocate(B(m,n))
-allocate(x(m))
-allocate(y(n))
-
-
-A(1,1) = 0
-A(2,1) = 0
-A(1,2) = 1
-A(2,2) = 0
-
-B(1,1) = 0
-B(2,1) = 0
-B(1,2) = -1
-B(2,2) = 0
-
-!~ B = -A
-!~ 
-!~ B = A
-!~ A = -B
-
-print*,'A'
-do i=1,m
-    print*,A(i,:)
-end do
-print*,'B'
-do i=1,m
-    print*,B(i,:)
-end do
-call find1nash_lemhow_real2(A,B,m,n,x,u,y,v)
-
-print*,' '
-print*,'x=',x
-print*,'u=',u
-print*,'y=',y
-print*,'v=',v
-
-
-
 call find1_perfect_subnash(moneyinit,natendpayoff,behavstrat1)
 call find1_perfect_subnash(moneyinit,augendpayoff,behavstrat2)
+
+call sumproba_behavestrat(moneyinit,behavstrat1)
+call sumproba_behavestrat(moneyinit,behavstrat2)
+
+print*,'Let the game begin'
+
+print*,behavstrat1(50,0,50,:)
+
+call play_game(human,behav_distrib1,winner,verball)
+
+!~ ngames = 10**7
+
+!~ call play_many_games(behav_distrib1,behav_distrib2,ngames,winnertable)
+
+!~ rwinnertable = real(winnertable,real_kind)
+!~ rwinnertable = rwinnertable / ngames 
+
+!~ print *,rwinnertable*100
+
 
--- src/Strategies/strategies.f90
+++ src/Strategies/strategies.f90
@@ -177,6 +177,7 @@ contains
         integer                 , dimension(n+m)                    :: bas
         real(kind=real_kind)                                        :: moffset
         real(kind=real_kind)                                        :: p,q, piv
+        real(kind=real_kind)                        , parameter     :: eps = 1d-10
         integer                                     , parameter     :: ko =  1  ! Missing label.
         integer                                                     :: k,l,i,j
         integer                                                     :: mpn,mp1,mp2
@@ -197,8 +198,8 @@ contains
             end do
         end do
         
-!~         moffset = moffset - 1
-        moffset =  - 2
+        moffset = moffset - 1
+!~         moffset =  - 2
         
         do i=1,m
             do j=1,m
@@ -246,20 +247,27 @@ contains
 
             ! Determines picked up label !
             l = mp1
-            do while (mat(l,k) .le. 0_real_kind)
+            do while (mat(l,k) .le. eps)
                 l = l + 1
             end do
-            p = c(l) / mat(l,k)
             do i=(l+1),mpn
-                if (mat(i,k) > 0) then
-                    if (c(i) < (p*mat(i,k))) then
-                        p = c(i) / mat(i,k)
-                        l = i
+                if (mat(i,k) > eps) then
+                    p = mat(i,k)*c(l) - mat(l,k)*c(i)
+                    if (p > 0) then
+                        if (p < eps) then
+                            if (bas(i) < bas(l)) then
+                                l=i
+                            end if
+                        else
+                            l=i
+                        end if
                     end if
                 end if
             end do
-            c(l) = p                                                
+
+
             ! Pivoting arround l
+            c(l) = c(l) / mat(l,k)
             p = mat(l,k)
             do i=1,k-1
                 mat(l,i) = mat(l,i) / p
@@ -292,20 +300,26 @@ contains
 
             ! Determines picked up label
             l = 1
-            do while (mat(l,k) .le. 0_real_kind)
-                l = l + 1 
+            do while (mat(l,k) .le. eps)
+                l = l + 1
             end do
-            p = c(l) / mat(l,k)
             do i=(l+1),m
-                if (mat(i,k) > 0) then
-                    if (c(i) < (p*mat(i,k))) then
-                        p = c(i) / mat(i,k)
-                        l = i
+                if (mat(i,k) > eps) then
+                    p = mat(i,k)*c(l) - mat(l,k)*c(i)
+                    if (p > 0) then
+                        if (p < eps) then
+                            if (bas(i) < bas(l)) then
+                                l=i
+                            end if
+                        else
+                            l=i
+                        end if
                     end if
                 end if
             end do
-            c(l) = p                                                
+            
             ! Pivoting arround l
+            c(l) = c(l) / mat(l,k)
             p = mat(l,k)
             do i=1,k-1
                 mat(l,i) = mat(l,i) / p
@@ -383,243 +397,6 @@ contains
 
     end subroutine find1nash_lemhow_real
 
-! Finds a Nash equilibrium of a strategic form game with real payoffs using the Lemke-Howson algorithm.
-    subroutine find1nash_lemhow_real2(B,A,m,n,x,v,y,u)
-        
-        real(kind=real_kind)    , dimension(m,n)    , intent(in)    :: A    ! Payoff matrix : m lines, n columns
-        real(kind=real_kind)    , dimension(m,n)    , intent(in)    :: B    ! Payoff matrix : n lines, m columns
-        integer                                     , intent(in)    :: m    ! Number of lines of A = number of pure strategies of player X
-        integer                                     , intent(in)    :: n    ! Number of columns of A = number of pure strategies of player Y
-        real(kind=real_kind)    , dimension(m)      , intent(out)   :: x    ! Scaled probabilities to play pure strategies for player X
-        real(kind=real_kind)                        , intent(out)   :: u    ! Expected payoff for player X
-        real(kind=real_kind)    , dimension(n)      , intent(out)   :: y    ! Scaled probabilities to play pure strategies for player Y
-        real(kind=real_kind)                        , intent(out)   :: v    ! Expected payoff for player X
-        
-        real(kind=real_kind)    , dimension(n+m,n+m)                :: mat
-        real(kind=real_kind)    , dimension(n+m)                    :: c
-        integer                 , dimension(n+m)                    :: bas
-        real(kind=real_kind)                                        :: moffset
-        real(kind=real_kind)                                        :: p,q, piv
-        real(kind=real_kind)                        , parameter     :: eps = 1d-11
-        integer                                     , parameter     :: ko =  1  ! Missing label.
-        integer                                                     :: k,l,i,j
-        integer                                                     :: mpn,mp1,mp2
-        
-        ! Initialisation
-        
-        mpn = m + n
-        mp1 = m + 1
-        mp2 = m + 2
-
-        moffset=1d25
-        do i=1,m
-            do j=1,n
-                p = min(A(i,j),B(i,j))
-                if (moffset > p) then
-                    moffset = p
-                end if
-            end do
-        end do
-        
-!~         moffset = moffset - 1
-        moffset =  - 2
-        
-        do i=1,m
-            do j=1,m
-                if (i .ne. j) then
-                    mat(i,j) = 0
-                else
-                    mat(i,j) = 1
-                end if
-            end do
-            do j=mp1,mpn
-                mat(i,j) = A(i,j-m) - moffset
-            end do
-        end do
-        do i=mp1,mpn
-            do j=1,m
-                mat(i,j) = B(j,i-m) - moffset
-            end do
-            do j=mp1,mpn
-                if (i .ne. j) then
-                    mat(i,j) = 0
-                else
-                    mat(i,j) = 1
-                end if
-            end do
-        end do
-        
-        do i=1,mpn
-            bas(i) = i
-        end do
-        
-        c = 1
-        k = ko
-        ! Treat picked up label until missing label is found
-        mainloop:&
-        do
-
-!~             print*,'k=',k
-!~             print*,'bas=',bas
-!~             print*,'mat = '
-!~             do i=1,mpn
-!~                 print*,mat(i,:)
-!~             end do
-            
-
-
-            ! Determines picked up label !
-            l = mp1
-            do while (mat(l,k) .le. eps)
-                l = l + 1
-            end do
-            do i=(l+1),mpn
-                if (mat(i,k) > eps) then
-                    p = mat(i,k)*c(l) - mat(l,k)*c(i)
-                    if (p > 0) then
-                        if (p < eps) then
-                            if (bas(i) < bas(l)) then
-                                l=i
-                            end if
-                        else
-                            l=i
-                        end if
-                    end if
-                end if
-            end do
-
-
-            ! Pivoting arround l
-            c(l) = c(l) / mat(l,k)
-            p = mat(l,k)
-            do i=1,k-1
-                mat(l,i) = mat(l,i) / p
-            end do
-            mat(l,k) = 1
-            do i=k+1,mpn
-                mat(l,i) = mat(l,i) / p
-            end do
-            do i=mp1,l-1
-                p = mat(i,k)
-                do j=1,mpn
-                    mat(i,j) = mat(i,j) - p * mat(l,j)
-                end do 
-                c(i) = c(i) - p*c(l)
-            end do
-            do i=l+1,mpn
-                p = mat(i,k)
-                do j=1,mpn
-                    mat(i,j) = mat(i,j) - p * mat(l,j)
-                end do
-                c(i) = c(i) - p*c(l)                    
-            end do
-
-            i = bas(l)
-            bas(l) = k  
-            k = i
-            if (k == ko) then
-                exit mainloop
-            end if         
-
-            ! Determines picked up label
-            l = 1
-            do while (mat(l,k) .le. eps)
-                l = l + 1
-            end do
-            do i=(l+1),m
-                if (mat(i,k) > eps) then
-                    p = mat(i,k)*c(l) - mat(l,k)*c(i)
-                    if (p > 0) then
-                        if (p < eps) then
-                            if (bas(i) < bas(l)) then
-                                l=i
-                            end if
-                        else
-                            l=i
-                        end if
-                    end if
-                end if
-            end do
-            
-            ! Pivoting arround l
-            c(l) = c(l) / mat(l,k)
-            p = mat(l,k)
-            do i=1,k-1
-                mat(l,i) = mat(l,i) / p
-            end do
-            mat(l,k) = 1
-            do i=k+1,mpn
-                mat(l,i) = mat(l,i) / p
-            end do
-            do i=1,l-1
-                p = mat(i,k)
-                do j=1,mpn
-                    mat(i,j) = mat(i,j) - p * mat(l,j)
-                end do
-                c(i) = c(i) - p*c(l)
-            end do
-            do i=l+1,m
-                p = mat(i,k)
-                do j=1,mpn
-                    mat(i,j) = mat(i,j) - p * mat(l,j)
-                end do
-                c(i) = c(i) - p*c(l)
-            end do
-
-
-            ! k enters the basis, l leaves the basis
-            i = bas(l)
-            bas(l) = k  
-            k = i
-            if (k == ko) then
-                exit mainloop
-            end if         
-        end do mainloop
-        
-        x = 0
-        y = 0
-        do i=mp1,mpn
-            if (bas(i) < mp1) then
-                x(bas(i)) = c(i)
-            end if
-        end do
-        do i=1,m
-            if (bas(i) > m) then
-                y(bas(i)-m) = c(i)
-            end if
-        end do
-        
-        
-        u = 0
-        do i=1,m
-            u = u + x(i)
-        end do
-        u = 1/u
-        x = x*u
-
-        v = 0
-        do i=1,n
-            v = v + y(i)
-        end do
-        v = 1/v
-        y = y*v
-
-        u=0
-        do i=1,m
-            do j=1,n
-                u = u + x(i)*B(i,j)*y(j)
-            end do
-        end do
-        
-        v=0
-        do i=1,m
-            do j=1,n
-                v = v + x(i)*A(i,j)*y(j)
-            end do
-        end do
-
-    end subroutine find1nash_lemhow_real2
-
 ! Finds a perfect subgame Nash equilibrium for the mercenaires game
     subroutine find1_perfect_subnash(moneymax,payofffun,behavstrat)
     
@@ -681,7 +458,7 @@ contains
 !~                         print*,rpayoff(i,1:rmon)
 !~                     end do
                                     
-                    call find1nash_lemhow_real2(            &
+                    call find1nash_lemhow_real(            &
                      rpayoff(1:lmon,1:rmon)                 &
                     ,lpayoff(1:lmon,1:rmon)                 &
                     ,lmon           ,rmon                   &
@@ -700,6 +477,58 @@ contains
         
     
     end subroutine find1_perfect_subnash
+
+! Play according to the first behavior distribution
+    function behav_distrib1() result(pay)
+    
+        integer                                             :: pay
+        integer , dimension(3)                              :: state
+        
+        call copy_current_board_left(state)
+
+        print*,1,state        
+        call play_distrib(behavstrat1(state(1),state(2),state(3),1:state(1))    &
+                            ,state(1),pay)
+    
+    end function behav_distrib1
+    
+    function behav_distrib2() result(pay)
+    
+        integer                                             :: pay
+        integer , dimension(3)                              :: state
+        
+        call copy_current_board_left(state)
+        
+        print*,2,state
+        call play_distrib(behavstrat2(state(1),state(2),state(3),1:state(1))    &
+                            ,state(1),pay)
+    
+    end function behav_distrib2
+
+! Sums probabilities to buid cdf from pdf
+    subroutine sumproba_behavestrat(moneymax,behavstrat)
+    
+        integer                 , intent(in)    :: moneymax
+        real(kind=real_kind)            ,   &
+        dimension(   1:moneymax         ,   &
+        (-disttocen+1):(disttocen-1)    ,   &
+        1:moneymax                      ,   &
+        0:moneymax                         )&
+                                , intent(inout) :: behavstrat
+        
+        integer                                 :: i,j,k,l
+        
+        do i=1,moneymax
+            do j=1,moneymax
+                do k=(1-disttocen),(disttocen-1)
+                    do l=2,moneymax
+                        behavstrat(i,k,j,l) = behavstrat(i,k,j,l) + behavstrat(i,k,j,l-1)
+                    end do
+                end do
+            end do
+        end do
+    
+    end subroutine sumproba_behavestrat
 
 ! Natural end payoff function
     function natendpayoff(lmon,mpos,rmon) result (p)
@@ -768,3 +597,4 @@ contains
     end function augendpayoff
     
 end module strategies
+
